0f532c4e016c866d80cf572a43cbe48362d9df1d
shagoon kedia <shagoon.kedia@epicor.com>
2024-12-03 12:36:27 +0530

    Second program

---
+++ src/Vowels.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Vowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String input = sc.nextLine();
+        char ch;
+        int count = 0;
+
+        
+
+    //     if(input.toLowerCase().matches(".*[aeiou].*")){
+           
+    //         System.out.println("Vowel is present");
+    //     }
+    //     else {
+    //         System.out.println("Vowel is not present");
+    //     }
+    // }
+
+    for(int i = 0; i < input.length(); i++){
+        ch = input.charAt(i);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+            count++;
+        }
+    }
+    if(count > 0){
+        System.out.println(count + " Vowels are present");
+    }
+    else {
+        System.out.println("Vowel is not present");
+    }
+    
+}
+}
+
+
+
