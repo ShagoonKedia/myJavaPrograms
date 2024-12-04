8ca75b138b1cbf33f939d016182f7be0d1597bd4
shagoon kedia <shagoon.kedia@epicor.com>
2024-12-04 22:36:59 +0530

    fourth program

---
+++ src/PalindromeString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a string");
+
+    String str = sc.nextLine();
+
+    System.out.println(palindrome(str));
+        
+
+    }
+    public static boolean palindrome(String s){
+        int n = s.length();
+        boolean result = true;
+        for(int i=0;i<n/2;i++){
+            if(s.charAt(i) != s.charAt(n-i-1)){
+                result =  false;
+                break;
+            }
+        }
+        return result;
+    }
+}
