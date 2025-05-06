3128298d94ab996cb4769acad4c8ab73b0955aef
shagoon kedia <shagoon.kedia@epicor.com>
2025-05-06 11:09:10 +0530

    New program

---
+++ src/Arrays/BuyOrSellStocks2.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class BuyOrSellStocks2 {
+    public static void main(String[] args) {
+
+        int[] prices = {7,1,5,3,6,4};
+
+        System.out.println(maxProfit(prices));
+        
+    }
+
+    public static int maxProfit(int[] prices){
+
+        if(prices == null || prices.length < 2){
+            return 0;
+        }
+
+       
+        int profit = 0;
+        
+        for(int i = 1; i < prices.length; i++){
+
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+
+
+}
+
+
